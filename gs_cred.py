88e07ab20e474c38945d84f50d5d2a28c47d2bf0
logvas <thelogvasstudio@gmail.com>
2024-02-07 14:53:30 +0000

    minimal code

--- gs_cred.py
+++ gs_cred.py
@@ -5,19 +5,7 @@ scope = [ 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive"]
 credentials = ServiceAccountCredentials.from_json_keyfile_name('gs_credentials.json', scope)
 client = gspread.authorize(credentials)
-
-sh = gc.open_by_url(https://docs.google.com/spreadsheets/d/1vquI_TsT5Lkf0NrTcEtcEq2j2TqIv_GSOvqjPZmU5pk/edit?usp=sharing)
-worksheet = sh.get_worksheet(0)    
-val = worksheet.acell('B1').value
-
-
-#sheet = client.create("FirstSheet")
-#sheet.share('thelogvasstudio@gmail.com', perm_type='user', role='writer')
-
 sheet = client.open("FirstSheet").sheet1
-
-print(sheet.acell('A9').value)
-print(val)
-num=2
-sheet.update('A'+str(num), val)
+num=4
+sheet.update('B'+str(num), "2120")
 
